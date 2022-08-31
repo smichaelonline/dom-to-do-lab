6cdb30a212a28d51bc6e3770f4a3bf57b6804e67
smichaelonline <stephaniemichael001@gmail.com>
2022-08-31 18:12:35 -0400

    complete dom lab

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 // create three cached element references for each element w/ and id in HTML (input, button & ul)
 //* cached element references 
-const input = document.getElementById('input')
+const inputEl = document.querySelector('#input-box')
 const button = document.querySelector ('#submit-button')
 const uList = document.querySelector('#todo-list')
+
+console.dir(inputEl)
+console.dir(button)
+console.dir(uList)
 
 //* event listener 
 button.addEventListener('click', function(evt){
   const li = document.createElement('li')
-  li.textContent = input.value
+  li.textContent = inputEl.value
+  uList.appendChild(li)
+  inputEl.value = ' '
 })
+
 //add an event listener that will listen for click events 
 //when the button is clicked - we will create a new <li> element
 // set the <li> text to what the user has entered in the input element  
